4c2ff926554b17cade4dde10c9a85a67ee498db1
Gerardo <gera_garciag58@unicah.edu>
2026-07-04 22:49:54 -0600

    build

---
+++ src/data/pages/index_page.js
@@ -0,0 +1,28 @@
+export default {
+    planes: [
+        {
+            card_title: "Carta 1",
+            card_description: "Tareas básicas y límite de 30 horas vcpu.",
+            card_img: "https://picsum.photos/id/156/300/200",
+            card_label: "Para novatos",
+            card_price: "120.00",
+            card_cta: "Comprar",
+        },
+        {
+            card_title: "Carta 2",
+            card_description: "100 horas",
+            card_img: "https://picsum.photos/id/256/300/200",
+            card_label: "para profecionales",
+            card_price: "1000.00",
+            card_cta: "Comprar",
+        },
+        {
+            card_title: "Carta 3",
+            card_description: "Mensual.",
+            card_img: "https://picsum.photos/id/56/300/200",
+            card_label: "negocio serio",
+            card_price: "50,000.00",
+            card_cta: "Comprar",
+        }
+    ]
+};
